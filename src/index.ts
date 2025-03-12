d69bd405ccef09ee2f9cc69166d34e171d678ecd
Aryan <bhardwaj7a@gmail.com>
2025-03-12 21:47:04 +0530

    Deleting a User

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient(); // generate an instance of Prisma Client Class
 
-async function createUser() {
-   await client.user.create({
-    data : {
-        username : "harkirat",
-        password : "123123",
-        age : 21,
-        city : "Delhi"
+async function deleteUser() {
+   await client.user.delete({
+    where:{
+        id:1
     }
 })}
 
-createUser();
+deleteUser();
