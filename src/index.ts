3499d6b0130220d70f0ddb1c914bc4ae022b3459
Aryan <bhardwaj7a@gmail.com>
2025-03-12 21:45:15 +0530

    Creating a user using prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient(); // generate an instance of Prisma Client Class
+
+async function createUser() {
+   await client.user.create({
+    data : {
+        username : "harkirat",
+        password : "123123",
+        age : 21,
+        city : "Delhi"
+    }
+})}
+
+createUser();
