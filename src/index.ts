0686cdd73fdb231a84b4e8117da29726b3767907
Aryan <bhardwaj7a@gmail.com>
2025-03-12 21:51:41 +0530

    Updating an user

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient(); // generate an instance of Prisma Client Class
 
-async function deleteUser() {
-   await client.user.delete({
-    where:{
-        id:1
+async function updateUser() {
+   await client.user.update({
+    where : {
+        id : 2
+    },
+    data : {
+        username : "Aryan",
+        password : "123123",
+        age : 21,
+        city : "Delhi"
     }
 })}
 
-deleteUser();
+updateUser();
