2f5e54e5997c46b60a8428ffbaa0463da255cfc6
Aryan <bhardwaj7a@gmail.com>
2025-03-12 22:35:21 +0530

    Expressify the application

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient(); // generate an instance of Prisma Client Class
+const app = express();
+app.use(express.json())
 
-async function readUser() {
-   const user = await client.user.findFirst({
-    where : {
-        id : 2
-    },
-    select:{
-      todos : true // i can include todos here
-    }
+app.get("/users" , async (req,res) => {
+    const users = await client.user.findMany();
+    res.json({
+        users
+    })
 })
-console.log(user)
-}
 
-readUser();
+app.get("/todos/:id" , async(req,res) => {
+    const id = req.params.id as unknown as number;
+    const users = await client.user.findFirst({
+        where : {
+            id : id
+        },
+       select : {
+            todos : true
+        }
+    });
+    res.json({
+        users
+    })
+})
+
+app.listen(3000);
