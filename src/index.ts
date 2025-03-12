5bf2a0e341a362fa4934d3f5ffbe59e5bcdeb2c7
Aryan <bhardwaj7a@gmail.com>
2025-03-12 22:39:23 +0530

    Fix the type of request parameter

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,10 @@ app.get("/users" , async (req,res) => {
 })
 
 app.get("/todos/:id" , async(req,res) => {
-    const id = req.params.id as unknown as number;
+    const id = req.params.id;
     const users = await client.user.findFirst({
         where : {
-            id : id
+            id : parseInt(id)
         },
        select : {
             todos : true
